e5c548b6a01f63e283efb34ed7864446c2d6cafb
Tim K <tim750233@gmail.com>
2015-05-29 21:56:02 +0000

    precompiled locally

---
+++ public/assets/progress_update-c74263be7c86e6fce86927edaef16b81.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var pusher = new Pusher('5d80c1b3908b01bea17d');
+  var channel = pusher.subscribe('100');
+  channel.bind('update', function(data) {
+    var messageBox = $('#progress-status').children('.messages');
+    messageBox.html(data.message);
+
+    // var progressBar = $('#realtime-progress-bar');
+    // progressBar.width(data.progress+"%")
+
+  });
+});
